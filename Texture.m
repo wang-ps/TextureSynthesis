35155364b053277d923d458dff839b7cb39bcc9c
wang-ps <wangps@foxmail.com>
2014-07-31 16:47:41 +0800

    more results

--- Texture.m
+++ Texture.m
@@ -1,6 +1,6 @@
   %% input sample texture 
-  Z = imread('.\Image\Texture-01.png');
-  %   Z = imread('.\rst\InvTexture-02_rst.jpg');
+%    Z = imread('.\Image\Texture-01.png');
+     Z = imread('.\rst\InvTexture-02_rst.jpg');
   
   % build pyramid
   Zp{1} = Z;
@@ -13,7 +13,7 @@
   %% level 2 ( low resolution )
   mx = 200;
   nx = 200;
-  w = 8;
+  w = 4;
   
   disp('-----------');
   disp('level 2, low resolution');
@@ -31,8 +31,8 @@
   end
   
   % start level 2
-  X = TextureSynthesis(Zp{2}, mx/2, nx/2, 8, 5, X);
-  X = TextureSynthesis(Zp{2}, mx/2, nx/2, 4, 5, X);
+  X = TextureSynthesis(Zp{2}, mx/2, nx/2, 5, 5, X);
+  X = TextureSynthesis(Zp{2}, mx/2, nx/2, 3, 5, X);
 
   %% level 1
   disp('-----------');
@@ -44,7 +44,7 @@
   X = 4*imfilter(X1, fspecial('gaussian'), 'replicate');
 
 %   X = TextureSynthesis(Z, mx, nx, 8, 3, X);
-  X = TextureSynthesis(Z, mx, nx, 12, 10, X);
+%   X = TextureSynthesis(Z, mx, nx, 12, 10, X);
   X = TextureSynthesis(Z, mx, nx, 8, 10, X);
   X = TextureSynthesis(Z, mx, nx, 4, 10, X);
   X = TextureSynthesis(Z, mx, nx, 2, 10, X);
